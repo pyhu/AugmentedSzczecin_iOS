4d824d3497b4a0c337fea13959058bbd3e263af0
Tomasz Szulc <tomasz.szulc@blstream.com>
2015-02-23 15:31:42 +0100

    Add AugmentedSzczecin project.

---
+++ AugmentedSzczecin/AugmentedSzczecin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AugmentedSzczecin
+//
+//  Created by Tomasz Szulc BLStream on 23/02/15.
+//  Copyright (c) 2015 BLStream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
