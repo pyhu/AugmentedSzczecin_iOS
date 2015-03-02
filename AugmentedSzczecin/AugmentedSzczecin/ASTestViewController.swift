51b1197857b3f15695b577e707ecb755c4b6977e
Grzegorz Pawlowicz <grzegorz.pawlowicz@me.com>
2015-03-03 00:05:29 +0100

    Readded BLSAC project + linker flags + ASTestViewController

---
+++ AugmentedSzczecin/AugmentedSzczecin/ASTestViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ASTestViewController.swift
+//  AugmentedSzczecin
+//
+//  Created by Grzegorz Pawlowicz on 02.03.2015.
+//  Copyright (c) 2015 BLStream. All rights reserved.
+//
+
+import UIKit
+
+class ASTestViewController: BLSAugmentedViewController {
+
+}
---
+++ AugmentedSzczecin/AugmentedSzczecin/AugmentedSzczecin-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  AugmentedSzczecin-Bridging-Header.h.h
+//  AugmentedSzczecin
+//
+//  Created by Grzegorz Pawlowicz on 02.03.2015.
+//  Copyright (c) 2015 BLStream. All rights reserved.
+//
+
+#import <BLSAugmentedController/BLSAugmentedController.h>
