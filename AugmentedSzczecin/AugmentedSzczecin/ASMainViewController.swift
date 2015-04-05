031e48c6ca152b12bb86f65ddf36ef66d2c5a8fc
Kacper Tłusty <kacper.tlusty@outlook.com>
2015-04-05 12:07:25 +0200

    Remove unused viewDidLoad method

--- AugmentedSzczecin/AugmentedSzczecin/ASMainViewController.swift
+++ AugmentedSzczecin/AugmentedSzczecin/ASMainViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ASMainViewController: BLSAugmentedViewController, BLSAugmentedViewControllerDelegate {
     
-    override func viewDidLoad() {
-    }
-    
     override func viewWillAppear(animated: Bool) {
         self.setMapRegionWithTopLeftCoordinate(CLLocationCoordinate2DMake(53.4286829,14.5559835), andBottomRightCoordinate: CLLocationCoordinate2DMake(53.4286829,14.5559835), animated: false)
     }
     
     func augmentedViewController(augmentedViewController: BLSAugmentedViewController!, viewForAnnotation annotation: BLSAugmentedAnnotation!, forUserLocation location: CLLocation!, distance: CLLocationDistance) -> BLSAugmentedAnnotationView! {
-        var view = BLSAugmentedAnnotationView()
-        return view
+        return BLSAugmentedAnnotationView()
     }
     
 }
